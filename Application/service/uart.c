7ecd570e19914b84eb5762412e70f85a71e31813
whwu <whwu@appotronics.cn>
2022-02-18 10:09:00 +0800

    add uart.c. add CmdlineRegister.c add empty uart.c

--- Application/service/uart.c
+++ Application/service/uart.c
@@ -1,14 +1,11 @@
 /**
- * @file cmd.c
+ * @file uart.c
  * @author Wu Wenhao (whwu@appotronics.com)
  * @brief 
  * @version 1.02
- * @date 2022-02-15
+ * @date 2022-02-17
  * 
  * @copyright Copyright@appotronics 2022. All Rights Reserved
  * 
  */
-
-#include "main.h"
-#include "Cmdline.h"
 
