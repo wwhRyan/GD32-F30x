a5891cac1684e016238cfd17a229fd1f81120645
whwu <whwu@appotronics.cn>
2022-07-26 19:21:06 +0800

    add many CMakeLists.txt for clang lint. still have some problems in files. "In included file: unknown type name 'uint32_t'".

---
+++ cmake/example_add_libs.cmake
@@ -0,0 +1,15 @@
+add_subdirectory(${PROJECT_SOURCE_DIR}/Firmware Firmware)
+add_subdirectory(${PROJECT_SOURCE_DIR}/Utilities Utilities)
+add_subdirectory(${PROJECT_SOURCE_DIR}/BoardApp BoardApp)
+add_subdirectory(${PROJECT_SOURCE_DIR}/PeriphFunc PeriphFunc)
+add_subdirectory(${PROJECT_SOURCE_DIR}/ulog ulog)
+
+target_include_directories(${EXEC_NAME}_CMSIS PRIVATE .)
+target_include_directories(${EXEC_NAME}_standard_peripherals PRIVATE .)
+target_include_directories(${EXEC_NAME}_periph_func PRIVATE .)
+target_include_directories(${EXEC_NAME}_utils PRIVATE .)
+target_include_directories(${EXEC_NAME}_lcos_board PRIVATE .)
+
+target_link_libraries(
+  ${EXEC_NAME} ${EXEC_NAME}_CMSIS ${EXEC_NAME}_standard_peripherals
+  ${EXEC_NAME}_periph_func ${EXEC_NAME}_utils ${EXEC_NAME}_lcos_board)
